b4c4e3f6c6be5966a9cfc6c8adc035162d217196
levblanc <levblanc@gmail.com>
2022-08-25 18:34:08 +0800

    Add data migration file

---
+++ migrations/data/01-lng-lat-to-loc.js
@@ -0,0 +1,25 @@
+const pg = require('pg');
+
+const pool = new pg.Pool({
+  host: 'localhost',
+  port: 5432,
+  database: 'socialnetwork',
+  user: 'levblancx',
+  password: '',
+});
+
+pool
+  .query(
+    `    
+      UPDATE posts
+      SET loc = POINT(lng, lat)
+      WHERE loc IS NULL; 
+    `
+  )
+  .then(() => {
+    console.log('Update complete');
+    pool.end();
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
